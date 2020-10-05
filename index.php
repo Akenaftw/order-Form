b72274ce84ae5a0adb2232ae6045f87c40281b38
Akena <felix.j.c.vanroost.com>
2020-10-05 11:12:04 +0200

    added if else to swap between drinks and food page

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 //this line makes PHP behave in a more strict way
 declare(strict_types=1);
 
@@ -16,21 +17,23 @@ function whatIsHappening() {
     var_dump($_SESSION);
 }
 
-//your products with their price.
+if (!isset($_GET["food"]) == 1 or $_GET["food"]){
 $products = [
     ['name' => 'Club Ham', 'price' => 3.20],
     ['name' => 'Club Cheese', 'price' => 3],
     ['name' => 'Club Cheese & Ham', 'price' => 4],
     ['name' => 'Club Chicken', 'price' => 4],
     ['name' => 'Club Salmon', 'price' => 5]
-];
-
+];}
+else{
 $products = [
     ['name' => 'Cola', 'price' => 2],
     ['name' => 'Fanta', 'price' => 2],
     ['name' => 'Sprite', 'price' => 2],
     ['name' => 'Ice-tea', 'price' => 3],
-];
+];}
+
+if (isset($_POST["email"]))
 
 $totalValue = 0;
 
