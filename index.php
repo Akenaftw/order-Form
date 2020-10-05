e3d04c753d861b012517634ff3add51feb4ed4a2
Akena <felix.j.c.vanroost.com>
2020-10-05 12:26:49 +0200

    pre break save, added some error messages and form validation

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ declare(strict_types=1);
 //we are going to use session variables so we need to enable sessions
 session_start();
 
-function whatIsHappening() {
+function whatIsHappening()
+{
     echo '<h2>$_GET</h2>';
     var_dump($_GET);
     echo '<h2>$_POST</h2>';
@@ -17,23 +18,54 @@ function whatIsHappening() {
     var_dump($_SESSION);
 }
 
-if (!isset($_GET["food"]) == 1 or $_GET["food"]){
-$products = [
-    ['name' => 'Club Ham', 'price' => 3.20],
-    ['name' => 'Club Cheese', 'price' => 3],
-    ['name' => 'Club Cheese & Ham', 'price' => 4],
-    ['name' => 'Club Chicken', 'price' => 4],
-    ['name' => 'Club Salmon', 'price' => 5]
-];}
-else{
-$products = [
-    ['name' => 'Cola', 'price' => 2],
-    ['name' => 'Fanta', 'price' => 2],
-    ['name' => 'Sprite', 'price' => 2],
-    ['name' => 'Ice-tea', 'price' => 3],
-];}
+if (!isset($_GET["food"]) == 1 or $_GET["food"]) {
+    $products = [
+        ['name' => 'Club Ham', 'price' => 3.20],
+        ['name' => 'Club Cheese', 'price' => 3],
+        ['name' => 'Club Cheese & Ham', 'price' => 4],
+        ['name' => 'Club Chicken', 'price' => 4],
+        ['name' => 'Club Salmon', 'price' => 5]
+    ];
+} else {
+    $products = [
+        ['name' => 'Cola', 'price' => 2],
+        ['name' => 'Fanta', 'price' => 2],
+        ['name' => 'Sprite', 'price' => 2],
+        ['name' => 'Ice-tea', 'price' => 3],
+    ];
+}
 
-if (isset($_POST["email"]))
+$email = $street = $streetnumber = $city = $zipcode = "";
+$emailErr = $streetErr = $streetnumberErr = $cityErr = $zipcodeErr = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = test_input($_POST["email"]);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Invalid email format";
+    }
+
+    $street = test_input($_POST["street"]);
+    if (!preg_match("/^[a-zA-Z-' ]*$/", $street)) {
+        $streetErr = "Only letters and white space allowed";
+    }
+
+    $streetnumber = test_input($_POST["streetnumber"]);
+
+    $city = test_input($_POST["city"]);
+    if (!preg_match("/^[a-zA-Z-' ]*$/", $city)) {
+        $cityErr = "Only letters and white space allowed";
+    }
+
+    $zipcode = test_input($_POST["zipcode"]);
+
+}
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
 $totalValue = 0;
 
